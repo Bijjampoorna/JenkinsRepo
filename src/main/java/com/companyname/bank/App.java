a091d88d26f416683fccbcce3f68f11cab9aee94
Bijjampoorna <121427201+Bijjampoorna@users.noreply.github.com>
2023-12-23 14:00:04 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Commit Change" );
+        System.out.println( "SCM Triggers Commit Change" );
     }
 }
