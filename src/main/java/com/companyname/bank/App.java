465fa56f5966facedb506a80f883f81ec57cc4ba
Bijjampoorna <121427201+Bijjampoorna@users.noreply.github.com>
2024-01-25 16:09:00 +0530

    code changes

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "SCM Triggers Commit Change  " );
+        System.out.println( "SCM Triggers Commit Changes for jenkins  " );
     }
 }
